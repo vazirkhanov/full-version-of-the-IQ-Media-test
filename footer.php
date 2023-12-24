3571f4afa991740c05857b5b29a951ba69ffbbeb
vazirkhanov <154065845+vazirkhanov@users.noreply.github.com>
2023-12-24 12:10:30 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,21 @@
+<footer class="footer d-none d-lg-block">
+  <div class="container">
+    <span class="text-muted">
+      <nav class="navbar fixed-bottom navbar-expand-lg bg-body-tertiary d-flex justify-content-center justify-content-sm-center">
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-2 mb-md-0 me-md-auto link-dark text-decoration-none "> WebNote © 2023 Company, Inc</a>
+        <ul class="nav col-md-4 ">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted" data-toggle="tooltip" data-placement="top" title="Описание подсказки">О нас</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted" data-toggle="tooltip" data-placement="top" title="Описание подсказки">Команда</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted" data-toggle="tooltip" data-placement="top" title="Описание подсказки">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted" data-toggle="tooltip" data-placement="top" title="Описание подсказки">Контакты</a></li>
+        </ul>
+      </nav> 
+    </span>
+  </div>
+</footer>
 
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
